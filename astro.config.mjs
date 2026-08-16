48ff439664912888b1efc8dc4cc02e2705b0d52b
ExpressBoda <info@expressboda.com>
2026-08-16 19:30:09 +0200

    Remove Vercel adapter and server output

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: 'https://expressboda-website-six.vercel.app',
-  output: 'server',
-  adapter: vercel(),
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'de', 'es', 'ru', 'uk']
